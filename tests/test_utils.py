1312c04cee0e97243bd45107958fd8f19e852e0c
rusov63 <rusov63@yahoo.com>
2023-03-12 14:19:30 +0400

    Домашнее задание №3 15.2

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
 from utils.task import Stack
 from utils.custom_queue import Queue
+from utils.linked_list import LinkedList
 
 class TestNode(unittest.TestCase):
 
@@ -21,6 +22,14 @@ class TestNode(unittest.TestCase):
         self.assertEqual(queue.dequeue(), 'data2')
         self.assertEqual(queue.dequeue(), 'data3')
         self.assertEqual(queue.dequeue(), None)
+
+    def test_LinkedList(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(ll.print_ll(), None)
 
 
 
